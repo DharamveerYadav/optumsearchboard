a02663b7b6f9497b3755324f5ffcd1363056d3ce
DharamveerYadav <47549645+DharamveerYadav@users.noreply.github.com>
2022-09-12 14:04:39 +0530

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import React, {useState} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
 import NavigationItems from './NavigationItems/NavigationItems';
 import Breadcrumb from './Breadcrumb/Breadcrumb';
 import Results from './Results/Results';
+import NavBar from './NavBar/NavBar';
 
 export const NavigationItemsArray = [
   "Home",
@@ -27,17 +29,20 @@ const selected = crumb => {
   return (
     <div className="App">
      <Header />
-     <nav style={{
+     <BrowserRouter>
+     <NavBar />
+     </BrowserRouter>
+     {/* <nav style={{
        height: '4rem'
      }}
      ><NavigationItems items={NavigationItemsArray}></NavigationItems>
-     </nav>
-     <div className="container2 section-container">
+     </nav> */}
+     {/* <div className="container2 section-container">
      <Breadcrumb crumbs={ crumbs } selected={ selected }  />
      <span className="fs-1 fw-bold mx-4">Search Results</span>
      <Results />
-     </div>
-    </div>
+     </div>*/}
+    </div> 
   );
 }
 
